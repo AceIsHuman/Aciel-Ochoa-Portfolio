449e666ed4e38d59fe80016b6761c0c7bed07c6d
AceIsHuman <menoman1995@yahoo.com>
2020-02-05 10:47:41 -0600

    create NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppBar, Container } from '@material-ui/core';
+import { GitHub, LinkedIn } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    paddingLeft: '0.5rem',
+    cursor: 'pointer'
+  }
+}));
+
+const NavBar = props => {
+  const classes = useStyles();
+  
+  return (
+    <AppBar style={{ display: 'flex'}}>
+      <Container style={{ alignSelf: 'flex-end', width: 'auto', margin: '0.5rem' }}>
+        <GitHub className={classes.icon} />
+        <LinkedIn className={classes.icon} />
+      </Container>
+    </AppBar>
+  )
+};
+
+export default NavBar;
