e0df9846aacbf316482b6a112d9a601b03bafa03
AceIsHuman <menoman1995@yahoo.com>
2020-02-05 14:59:00 -0600

    change icons and add Dev icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { AppBar, Container } from '@material-ui/core';
-import { GitHub, LinkedIn } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
+import GitHub from '../assets/icons/github';
+import LinkedIn from '../assets/icons/linkedin';
+import Dev from '../assets/icons/web-development';
 
 const useStyles = makeStyles(theme => ({
   icon: {
     paddingLeft: '0.5rem',
+    width: '2.8rem',
+  },
+  icon2: {
     cursor: 'pointer'
   }
 }));
@@ -14,10 +19,11 @@ const NavBar = props => {
   const classes = useStyles();
   
   return (
-    <AppBar position='static' style={{ display: 'flex'}}>
-      <Container style={{ alignSelf: 'flex-end', width: 'auto', margin: '0.5rem' }}>
-        <GitHub className={classes.icon} />
-        <LinkedIn className={classes.icon} />
+    <AppBar position='static'>
+      <Dev className={classes.icon} />
+      <Container style={{ display: 'flex', alignSelf: 'flex-end', width: 'auto', margin: '0.5rem' }}>
+        <GitHub className={`${classes.icon} + ${classes.icon2}`} />
+        <LinkedIn className={`${classes.icon} + ${classes.icon2}`} />
       </Container>
     </AppBar>
   )
