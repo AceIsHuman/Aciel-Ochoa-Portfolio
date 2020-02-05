419b2b0389784e6d84a81e0bc8b5982686ea9882
AceIsHuman <menoman1995@yahoo.com>
2020-02-05 15:00:29 -0600

    add attribute for icons in footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Box } from '@material-ui/core';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Container>
         <Header />
       </Container>
+      <Footer />
     </Box>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    </>
+  )
+};
+
+export default Footer;
