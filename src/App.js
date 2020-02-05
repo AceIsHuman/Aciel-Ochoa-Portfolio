968181c1c1987042fe907d7adbceaf26980e7c06
AceIsHuman <menoman1995@yahoo.com>
2020-02-05 11:08:02 -0600

    import Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Container, Box } from '@material-ui/core';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <Box>
       <NavBar />
+      <Container>
+        <Header />
+      </Container>
     </Box>
   );
 }
