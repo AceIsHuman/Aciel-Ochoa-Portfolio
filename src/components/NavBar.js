faba0ac9cc3c3a31cac294bdaff58df0c23a669b
AceIsHuman <menoman1995@yahoo.com>
2020-06-24 17:58:50 -0500

    wrap icons with Container

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,38 +8,48 @@ import Dev from '../assets/icons/web-development';
 const useStyles = makeStyles(theme => ({
   icon: {
     padding: '0.5rem',
-    width: '2.8rem'
+    width: '2.8rem',
   },
   link: {
-    cursor: 'pointer'
+    cursor: 'pointer',
   },
   appBar: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   linkContainer: {
     display: 'flex',
     alignSelf: 'flex-end',
     width: 'auto',
     margin: '0',
-    padding: '0'
-  }
+    padding: '0',
+  },
 }));
 
 const NavBar = props => {
   const classes = useStyles();
 
   return (
-    <AppBar className={classes.appBar} position='static'>
-      <Dev className={classes.icon} />
-      <Container className={classes.linkContainer}>
-        <a href='https://github.com/AceIsHuman' target='_blank' rel="noopener noreferrer">
-          <GitHub className={`${classes.icon} + ${classes.link}`} />
-        </a>
-        <a href='https://www.linkedin.com/in/aciel-ochoa' target='_blank' rel="noopener noreferrer">
-          <LinkedIn className={`${classes.icon} + ${classes.link}`} />
-        </a>
+    <AppBar position="static">
+      <Container className={classes.appBar}>
+        <Dev className={classes.icon} />
+        <Container className={classes.linkContainer}>
+          <a
+            href="https://github.com/AceIsHuman"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHub className={`${classes.icon} + ${classes.link}`} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/aciel-ochoa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedIn className={`${classes.icon} + ${classes.link}`} />
+          </a>
+        </Container>
       </Container>
     </AppBar>
   );
