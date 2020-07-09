5a885b3aa87bf5d9357422f6b1b6af676a93b263
AceIsHuman <menoman1995@yahoo.com>
2020-07-09 12:57:29 -0500

    add project info, description is currently lorem

---
+++ src/components/Projects/project_info.js
@@ -0,0 +1,10 @@
+import hivestack_image from '../../assets/projects/hivestack.gif';
+
+export const hivestack = {
+  name: 'Hive Stack',
+  deploy_url: 'https://hivestack-fe.herokuapp.com',
+  github_url: 'https://github.com/AceIsHuman/where-to-code-fe',
+  description: 'Direct mailing startup user experience handshake partnership value proposition. Leverage churn rate disruptive. Innovator launch party pivot pitch bootstrapping. Stealth focus funding crowdfunding startup customer graphical user interface. Advisor virality user experience analytics return on investment facebook product management branding supply chain. Alpha series A financing branding research & development beta analytics interaction design stealth long tail release graphical user interface. Pitch social proof twitter return on investment social media value proposition incubator research & development agile development business-to-consumer scrum project user experience hackathon non-disclosure agreement. Crowdfunding buyer twitter niche market business plan client partner network. Business model canvas holy grail business-to-business burn rate vesting period stealth twitter pitch seed round iteration metrics channels creative. Supply chain startup bootstrapping focus long tail.',
+  image: hivestack_image,
+  image_alt: 'view of hivestack deployment'
+}
