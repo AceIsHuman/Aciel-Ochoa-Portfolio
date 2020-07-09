cc92d180948f4da87b59b79507b4cc6d8f3e555c
AceIsHuman <menoman1995@yahoo.com>
2020-07-09 12:58:29 -0500

    create Projects component

---
+++ src/components/Projects/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+import { hivestack } from './project_info';
+import Project from './Project';
+
+function Projects() {
+  return (
+    <Box>
+      <h3>Projects</h3>
+      <div>
+        <Project project={hivestack} />
+      </div>
+    </Box>
+  );
+}
+
+export default Projects;
