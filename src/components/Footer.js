5048417c2ed04348cd2a0897d643574f7d312577
AceIsHuman <menoman1995@yahoo.com>
2020-07-12 13:15:55 -0500

    wrap in footer tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <>
+    <footer>
       Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-    </>
+    </footer>
   )
 };
 
