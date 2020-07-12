f8fb70a7f4075c174763fc5cf9f7e43f326598e7
AceIsHuman <menoman1995@yahoo.com>
2020-07-12 18:36:56 -0500

    change to h2

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Box } from "@material-ui/core";
+import React from 'react';
+import { Box } from '@material-ui/core';
 import { hivestack } from './project_info';
 import Project from './Project';
 
 function Projects() {
   return (
     <Box>
-      <h3>Projects</h3>
+      <h2>Projects</h2>
       <div>
         <Project project={hivestack} />
       </div>
