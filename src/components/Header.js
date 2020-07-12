fc3a66afb04fd7c5c76ed359d76712cf2080ea27
AceIsHuman <menoman1995@yahoo.com>
2020-07-12 13:17:20 -0500

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,50 @@
-import React from 'react'
-import { Typography, Box, Divider } from '@material-ui/core'
+import React from 'react';
+import { Typography, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import CreditBadge from '../assets/images/credit-badges/kevin-ku';
+import banner from '../assets/images/banner.jpg';
 
 const useStyles = makeStyles(theme => ({
-  box: {
-    minHeight: '50vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center'
+  imageTag: {
+    float: 'left',
+    position: 'absolute',
+    bottom: '0',
+    right: '0',
+    zIndex: 2,
   },
-  divider: {
-    marginBottom: '0.5rem'
-  }
+  imageContainer: {
+    position: 'relative',
+    minHeight: '100vh',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    backgroundImage: `url(${banner})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'bottom',
+    margin: '0',
+  },
+  text: {
+    color: 'rgb(231, 231, 236)',
+    position: 'absolute',
+    bottom: '0',
+  },
 }));
 
 const Header = props => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.box}>
-      <Typography variant='h2' gutterBottom>Aciel Ochoa</Typography>
-      <Divider className={classes.divider} variant='middle' />
-      <Typography variant='h4' gutterBottom>Full Stack Web Developer</Typography>
-    </Box>
-  )
+    <Container className={classes.imageContainer}>
+      <div className={classes.text}>
+        <Typography variant="h2">Aciel Ochoa</Typography>
+        <Typography variant="h4" gutterBottom>
+          Full Stack Web Developer
+        </Typography>
+      </div>
+      <div className={classes.imageTag}>
+        <CreditBadge />
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
