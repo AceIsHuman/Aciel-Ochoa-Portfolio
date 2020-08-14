852a31b595d8f4036593502998ea365fcc14c0a0
AceIsHuman <menoman1995@yahoo.com>
2020-08-14 16:13:32 -0500

    update HiveStack description

--- src/components/Projects/project_info.js
+++ src/components/Projects/project_info.js
@@ -5,7 +5,7 @@ export const hivestack = {
   name: 'Hive Stack',
   deploy_url: 'https://hivestack-fe.herokuapp.com',
   github_url: 'https://github.com/AceIsHuman/where-to-code-fe',
-  description: 'Finding out details about public places to work can be tedious. Get the information you need, and only that information, to work and code on the go. Hive Stack is a web application that helps users find wifi in their area using Google Maps Services. Front-end built using React with axios, redux,  & styled-components. Back-end built using express, PostgreSQL, jsonwebtoken, & nesthydration. Our mission is to connect WiFi users with local businesses who offer internet access catered to the user’s needs, paired with a comfortable working environment, while offering businesses a way to engage with a new customer audience.',
+  description: 'Finding out details about public places to work can be tedious. Get the information you need to work and code on the go. Hive Stack is a web application that helps users find wifi in their area using Google Maps Services. Front-end built using React with axios, redux,  & styled-components. Back-end built using express, PostgreSQL, jsonwebtoken, & nesthydration. Our mission is to connect WiFi users with local businesses who offer internet access catered to the user’s needs, paired with a comfortable working environment, while offering businesses a way to engage with a new customer audience.',
   image: hivestack_image,
   image_alt: 'view-of-hivestack-deployment'
 }
