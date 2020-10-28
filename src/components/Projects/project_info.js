08ecfafd953436ca72c3e7a97eb77327ee420f0e
AceIsHuman <menoman1995@yahoo.com>
2020-10-27 23:37:22 -0500

    update project summaries

--- src/components/Projects/project_info.js
+++ src/components/Projects/project_info.js
@@ -8,7 +8,7 @@ export const hivestack = {
   deploy_url: 'https://hivestack-fe.herokuapp.com',
   github_url: 'https://github.com/AceIsHuman/where-to-code-fe',
   description:
-    'Finding out details about public places to work can be tedious. Get the information you need to work and code on the go. Hive Stack is a web application that helps users find wifi in their area using Google Maps Services. Front-end built using React with axios, redux,  & styled-components. Back-end built using express, PostgreSQL, jsonwebtoken, & nesthydration. Our mission is to connect WiFi users with local businesses who offer internet access catered to the user’s needs, paired with a comfortable working environment, while offering businesses a way to engage with a new customer audience.',
+    'Finding out details about public places to work can be tedious. Get the information you need to work and code on the go. Hive Stack is a web application that helps users find wifi in their area using Google Maps Services. Front-end built using React with Axios, Redux,  & Styled-Components. Back-end built using express, PostgreSQL, jsonwebtoken, & nesthydration. Our mission is to connect WiFi users with local businesses who offer internet access catered to the user’s needs, paired with a comfortable working environment while offering businesses a way to engage with a new customer audience.',
   image: hivestack_image,
   image_alt: 'view-of-hivestack-deployment',
 };
@@ -17,7 +17,7 @@ export const gameOfLife = {
   name: "Conway's Game of Life",
   deploy_url: 'https://gameoflife-ace.netlify.app/',
   github_url: 'https://github.com/AceIsHuman/game-of-life--cellular-automata',
-  description: `Check out this classic computer science program replicated using the React framework! John Conway's "Game of Life" simulates cellular automaton by following a few set of rules for cells on a grid. In this simulation, a user may click on a cell in the grid to change it's state, alive or dead.`,
+  description: `Check out this classic computer science program replicated using the React framework! John Conway's "Game of Life" simulates cellular automaton by following a few set of rules for cells on a grid. In this simulation, a user may click on a cell in the grid to change its state, alive or dead.`,
   image: gameOfLife_image,
   image_alt: 'game-of-life-simulation',
 };
@@ -35,7 +35,7 @@ export const macroCalculator = {
   name: 'Macro Calculator Landing Page',
   deploy_url: 'https://bw-macro-calculator.github.io/bw-macro-calculator/',
   github_url: 'https://github.com/AceIsHuman/bw-macro-calculator',
-  description: `As a responsive marketing website for an app called Macro Calculator, this project demonstrates mastery of web fundamental skills in HTML, CSS, and JavaScript. By using semantic HTML with LESS and JavaScrip, we were able to create components that manipulate the DOM upon user interaction. This includes hover effects on images, a drop-down navigation menu for mobile views, a customer testimonial carousel, and more.`,
+  description: `As a responsive marketing website for an app called Macro Calculator, this project demonstrates mastery of web-fundamental skills in HTML, CSS, and JavaScript. By using semantic HTML with LESS and JavaScrip, we were able to create components that manipulate the DOM upon user interaction. This landing page includes hover effects on images, a drop-down navigation menu for mobile views, a customer testimonial carousel, and more.`,
   image: macroCalculator_image,
   image_alt: 'macro-calculator-gif',
 };
