fa84fe8a626e69494caa68db72029a6b9b3812ce
AceIsHuman <menoman1995@yahoo.com>
2021-02-23 00:02:10 -0600

    replace anchor tag with Link component

--- src/assets/images/credit-badges/kevin-ku.js
+++ src/assets/images/credit-badges/kevin-ku.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react';
+import Link from '../../../components/reuseables/Link';
 
 export default () => (
-  <a
+  <Link
     style={{
-      backgroundColor: "black",
-      color: "white",
-      textDecoration: "none",
-      padding: "4px 6px",
+      backgroundColor: 'black',
+      color: 'white',
+      textDecoration: 'none',
+      padding: '4px 6px',
       fontFamily:
         "-apple-system, BlinkMacSystemFont, 'San Francisco', 'Helvetica Neue', Helvetica, Ubuntu, Roboto, Noto, 'Segoe UI', Arial, sans-serif",
-      fontSize: "12px",
-      fontWeight: "bold",
-      lineHeight: "1.2",
-      display: "inline-block",
-      borderRadius: "3px"
+      fontSize: '12px',
+      fontWeight: 'bold',
+      lineHeight: '1.2',
+      display: 'inline-block',
+      borderRadius: '3px',
     }}
-    href="https://unsplash.com/@ikukevk?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
-    target="_blank"
-    rel="noopener noreferrer"
+    src="https://unsplash.com/@ikukevk?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
     title="Download free do whatever you want high-resolution photos from Kevin Ku"
+    newTab
   >
     <span
       style={{
-        display: "inline-block",
-        padding: "2px 3px"
+        display: 'inline-block',
+        padding: '2px 3px',
       }}
     >
       <svg
         style={{
-          height: "12px",
-          width: "auto",
-          position: "relative",
-          verticalAlign: "middle",
-          top: "-2px",
-          fill: "white"
+          height: '12px',
+          width: 'auto',
+          position: 'relative',
+          verticalAlign: 'middle',
+          top: '-2px',
+          fill: 'white',
         }}
         viewBox="0 0 32 32"
       >
@@ -43,11 +43,11 @@ export default () => (
     </span>
     <span
       style={{
-        display: "inline-block",
-        padding: "2px 3px"
+        display: 'inline-block',
+        padding: '2px 3px',
       }}
     >
       Kevin Ku
     </span>
-  </a>
+  </Link>
 );
